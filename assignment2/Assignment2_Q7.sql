26246eee4be69a647f9f0cd53d17cce6f2f0bd4a
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-04-29 15:46:12 +1200

    confused about context of question 7, but did it

--- assignment2/Assignment2_Q7.sql
+++ assignment2/Assignment2_Q7.sql
@@ -7,3 +7,8 @@ less than 25.
 Hint: (1) Use CheckedOut date to extract the year and month when the book is loaned; (2) Use
 strftime() function to deal with the year and month.
 */
+SELECT Gender, strftime('%Y',CheckedOut) AS "Year", strftime('%m',CheckedOut) AS "Month", COUNT(CheckedOut) AS "The Number of Loans"
+FROM Customer c JOIN Loan l ON c.CustomerID = l.CustomerID
+GROUP BY strftime('%m',CheckedOut)
+HAVING COUNT(CheckedOut) < 25
+ORDER BY Gender DESC, strftime('%Y',CheckedOut) ASC, strftime('%m',CheckedOut) ASC
