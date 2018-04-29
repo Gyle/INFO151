2bf620863b43318e0c9ae2d5f62d11ed64025837
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-04-29 16:45:06 +1200

    question 11 may be done but needs review. turns out question 6 is wrong, kim soing shouldnt be in the result

--- assignment2/Assignment2_Q11.sql
+++ assignment2/Assignment2_Q11.sql
@@ -3,3 +3,8 @@
 How many students with a total overdue fee greater than half the total overdue fee of "Barclay,
 Fletcher" (exclude "Barclay, Fletcher")?
 */
+SELECT LastName, FirstName--COUNT(CustomerID) AS "Total amount of students overdue fee > threshold", 
+FROM Customer NATURAL JOIN Loan
+WHERE LastName != "Barclay" AND OverdueFee > (SELECT SUM(OverdueFee)
+													 FROM Customer NATURAL JOIN Loan
+													 WHERE LastName = "Barclay" AND FirstName = "Fletcher") / 2
