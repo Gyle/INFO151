6baa9a210f811ed6cffb49719b9054e3708f55a5
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-04-29 17:43:32 +1200

    Did Q10

--- assignment2/Assignment2_Q10.sql
+++ assignment2/Assignment2_Q10.sql
@@ -3,4 +3,8 @@
 Use only a left outer join to find the customers who did not make any loan. Show the CustomerID, Full
 Name, and Major. Show only the rows contain “INFO” major only.
 */
-SELECT CustomerID, LastName || ', ' || FirstName AS 'Full Name', Major
+SELECT c.CustomerID AS 'Customer ID', LastName || ', ' || FirstName AS 'Full Name', Major
+FROM Customer c JOIN (SELECT *
+					  FROM Loan
+					  WHERE OverdueFee IS NULL) no_loan_table ON c.CustomerID = no_loan_table.CustomerID
+WHERE Major = 'INFO'
