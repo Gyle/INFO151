62083ffff965aa28662b1f9370a772cf8b5423ca
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-04-29 17:17:29 +1200

    started q10

--- assignment2/Assignment2_Q10.sql
+++ assignment2/Assignment2_Q10.sql
@@ -3,3 +3,4 @@
 Use only a left outer join to find the customers who did not make any loan. Show the CustomerID, Full
 Name, and Major. Show only the rows contain “INFO” major only.
 */
+SELECT CustomerID, LastName || ', ' || FirstName AS 'Full Name', Major
