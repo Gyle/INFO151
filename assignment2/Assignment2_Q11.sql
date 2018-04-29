73ecd82dbf4c426bd6f766f1a84d73ef9c3bfc56
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-04-29 17:01:59 +1200

    fixed up Q11.

--- assignment2/Assignment2_Q11.sql
+++ assignment2/Assignment2_Q11.sql
@@ -3,8 +3,10 @@
 How many students with a total overdue fee greater than half the total overdue fee of "Barclay,
 Fletcher" (exclude "Barclay, Fletcher")?
 */
-SELECT LastName, FirstName--COUNT(CustomerID) AS "Total amount of students overdue fee > threshold", 
-FROM Customer NATURAL JOIN Loan
-WHERE LastName != "Barclay" AND OverdueFee > (SELECT SUM(OverdueFee)
-													 FROM Customer NATURAL JOIN Loan
-													 WHERE LastName = "Barclay" AND FirstName = "Fletcher") / 2
+SELECT DISTINCT LastName || ', ' || FirstName AS 'Full Name', COUNT(OverdueFee) AS "Number of Overdue Fees", SUM(OverdueFee) AS "Overdue Fee"
+FROM Customer c JOIN Loan l ON c.CustomerID = l.CustomerID
+WHERE NOT LastName = "Barclay"
+GROUP BY LastName
+HAVING SUM (OverdueFee) > (SELECT SUM(OverdueFee)
+						   FROM Customer NATURAL JOIN Loan
+						   WHERE LastName = "Barclay" AND FirstName = "Fletcher") / 2
