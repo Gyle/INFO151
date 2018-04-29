d5faeb74847847452ce79a540dc4483898918c3d
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-04-29 17:14:27 +1200

    did Q12

--- assignment2/Assignment2_Q12.sql
+++ assignment2/Assignment2_Q12.sql
@@ -4,3 +4,4 @@ Use the SQL date modifier expression in chapter 44 of the Sqlite tutorial to add
 minutes, and 45 seconds to the date “2018-05-11 23:59:59”. This question does not require a
 database.
 */
+SELECT datetime('2018-05-11 23:59:59') AS "Original Date", datetime('2018-05-11 23:59:59', '3 days', '8 hours', '3 minutes', '45 seconds') AS "Modified Date"
