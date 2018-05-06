6fd7a61362b1d02e12d152b74039e3f2c85e8f8c
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-05-07 01:13:00 +1200

    modified question10 to check if right_table.customerID == null (loan table). because if it is null then they do not exist in the loan table. if they do not exist in the load table then that customer never made a loan

--- assignment2/Assignment2_Q10.sql
+++ assignment2/Assignment2_Q10.sql
@@ -4,5 +4,6 @@ Use only a left outer join to find the customers who did not make any loan. Show
 Name, and Major. Show only the rows contain “INFO” major only.
 */
 SELECT DISTINCT c.CustomerID, LastName || ', ' || FirstName AS 'Full Name', Major  -- used DISTINCT as there were duplicate names
-FROM Customer c LEFT OUTER JOIN Loan l ON c.CustomerID = l.CustomerID
-WHERE Major = 'INFO'
+FROM Customer c LEFT OUTER JOIN Loan l ON c.CustomerID = l.CustomerID 
+WHERE Major = 'INFO' AND l.CustomerID IS NULL  --by checking if right_table.CustomerID == null, we eliminate the inner join matching results.
+Order by c.CustomerID 
