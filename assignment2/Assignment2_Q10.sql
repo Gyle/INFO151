4905060145c58190f0cc2ba89205800ea06ae7a5
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-05-07 01:13:52 +1200

    removed order by ID in Q10

--- assignment2/Assignment2_Q10.sql
+++ assignment2/Assignment2_Q10.sql
@@ -6,4 +6,3 @@ Name, and Major. Show only the rows contain “INFO” major only.
 SELECT DISTINCT c.CustomerID, LastName || ', ' || FirstName AS 'Full Name', Major  -- used DISTINCT as there were duplicate names
 FROM Customer c LEFT OUTER JOIN Loan l ON c.CustomerID = l.CustomerID 
 WHERE Major = 'INFO' AND l.CustomerID IS NULL  --by checking if right_table.CustomerID == null, we eliminate the inner join matching results.
-Order by c.CustomerID 
